7c195175e7e42e6a90f45233ea2c7a51a7eee716
Egor <Y.manoylenko.fit.121.20@knute.edu.ua>
2022-02-11 12:17:52 +0200

    F commit

---
+++ Lab 1(11.02.22)/Lab 1(11.02.22)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_11._02._22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
